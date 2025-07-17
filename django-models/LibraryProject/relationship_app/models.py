5049168612f6a812e5d554ddbc8068f4cfdcb2b7
saadelbonix <saad.elboni2@gmail.com>
2025-07-17 20:23:09 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,31 +1,43 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name='books'
+    )
 
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author.name}"
+
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
 
+
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(
+        Library,
+        on_delete=models.CASCADE,
+        related_name='librarian'
+    )
 
     def __str__(self):
-        return self.name
+        return f"{self.name} (at {self.library.name})"
+
 
 
 from django.db import models
