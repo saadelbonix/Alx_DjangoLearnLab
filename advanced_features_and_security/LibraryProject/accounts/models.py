d5947d7003bda54f692a36f2969fe320a693e857
saadelbonix <saad.elboni2@gmail.com>
2025-07-24 22:50:28 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -14,10 +14,6 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        if not extra_fields.get("is_staff"):
-            raise ValueError("Superuser must have is_staff=True.")
-        if not extra_fields.get("is_superuser"):
-            raise ValueError("Superuser must have is_superuser=True.")
         return self.create_user(username, email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
