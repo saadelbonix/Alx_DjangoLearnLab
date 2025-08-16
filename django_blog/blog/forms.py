beb1f3114ccda9042c6ff6124380d49a05e886b0
saadelbonix <saad.elboni2@gmail.com>
2025-08-16 15:16:09 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,26 +1,34 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget  
 from .models import Post, Comment
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ("username", "email", "password1", "password2")
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ("username", "email")
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content', 'tags')
+        fields = ("title", "content", "tags")
+        widgets = {
+            "tags": TagWidget(),  
+        }
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('content',)
+        fields = ("content",)
